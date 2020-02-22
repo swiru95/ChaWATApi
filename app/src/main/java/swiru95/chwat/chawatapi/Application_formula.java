4430ba0be7d23e0d9e3d7f12db238abdcd317df7
swiru95 <kswidrak95@gmail.com>
2020-02-22 12:04:39 +0100

    Piąta wersja. Zmiana wyglądu, poprawione działanie.

--- app/src/main/java/swiru95/chwat/chawatapi/Application_formula.java
+++ app/src/main/java/swiru95/chwat/chawatapi/Application_formula.java
@@ -1,10 +1,14 @@
 package swiru95.chwat.chawatapi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 
@@ -19,14 +23,29 @@ public class Application_formula extends AppCompatActivity {
     }
 
     public void sendEmail(View view){
-
+        if(!(((EditText)findViewById(R.id.uri_box)).getText().toString().trim().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+") ||
+                !((EditText)findViewById(R.id.uri_box)).getText().toString().trim().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+\\.+[a-z]") ||
+                !((EditText)findViewById(R.id.uri_box)).getText().toString().trim().matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+\\.+[a-z]+\\.+[a-z]"))
+        ){
+            new AlertDialog.Builder(Application_formula.this)
+                    .setTitle("UWAGA")
+                    .setMessage("Błędnie podany adres e-mail!\nSprawdź proszę jego poprawność!")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    }).show();
+            return;
+        }
         Intent emailIntent = new Intent(Intent.ACTION_SENDTO,
-                Uri.fromParts("mailto", "chor.wat.edu.pl", null));
+                Uri.fromParts("mailto", "chor@wat.edu.pl", null));
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Rekrutacja");
         emailIntent.putExtra(
                 Intent.EXTRA_TEXT,
                 ((EditText)findViewById(R.id.mail_box)).getText().toString()+
-                "\nWysłano od: "+ ((EditText)findViewById(R.id.uri_box)).getText().toString()
+                "\n\nWysłano od: "+ ((EditText)findViewById(R.id.uri_box)).getText().toString()
         );
         startActivityForResult(emailIntent, 1);
     }
